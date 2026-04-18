700826e30f09d55a45b89eb9894697eec2044d03
kwanghan-bae <kwanghan.bae@gmail.com>
2026-04-18 17:02:28 +0900

    기능(코어): re-내보내기 새 계약 에서 패키지 루트

--- packages/2d-core/src/index.ts
+++ packages/2d-core/src/index.ts
@@ -1,2 +1,9 @@
 export { GameManifest, parseGameManifest } from './manifest';
 export type { GameManifestInput, GameManifestValue } from './manifest';
+
+export type { ForgeGameInstance, StartGameFn } from './game-instance';
+
+export { exposeTestHooks } from './test-hooks';
+export type { StandardTestHookSlots, TestHookSlots } from './test-hooks';
+
+export { createSaveEnvelopeSchema, SaveEnvelopeMeta } from './save-envelope';
